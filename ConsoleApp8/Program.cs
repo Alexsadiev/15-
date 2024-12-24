79598166b8894581ab2ac541d68f556cb8694575
Alexsadiev <sadievsania@gmail.com>
2024-12-24 09:30:35 +0200

    1
    1

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        
+        static void Func()
+        {
+            int i = 0;
+            do
+            {
+                Console.Write($"{20}\t");
+                i++;
+
+            } while (i<10);
+        }
+   
+
+        
+
+
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+
+    }
+}
